d2d3499e28e3f34111339c41322293c48b4d75ed
SaamueelProfetaTI92 <48030651+SaamueelProfetaTI92@users.noreply.github.com>
2019-08-04 23:55:50 -0300

    Alterado 23:55

---
+++ app/src/main/java/br/sp/senac/atividade/Janela_two.java
@@ -0,0 +1,14 @@
+package br.sp.senac.atividade;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Janela_two extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_janela_two);
+    }
+}
