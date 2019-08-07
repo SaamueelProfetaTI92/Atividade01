27ef5c697c0b00f632481c0d1fd32a1f31b13e06
samuel.pssantos <saamuka70@gmail.com>
2019-08-07 17:16:36 -0300

    Atualizado 17:16

---
+++ app/src/main/java/br/sp/senac/atividade/day.java
@@ -0,0 +1,14 @@
+package br.sp.senac.atividade;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class day extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day);
+    }
+}
---
+++ app/src/main/java/br/sp/senac/atividade/layout_login.java
@@ -0,0 +1,14 @@
+package br.sp.senac.atividade;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class layout_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_login);
+    }
+}
